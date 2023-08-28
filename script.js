1b27ee36063032a01510d2b52d899d0c01d5d0e5
Isaac Duong <isaacduong157@gmail.com>
2023-08-28 15:31:04 +1000

    Modify winning logic. Made it more specific

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ const displayController = (() => {
 
     const board = gameBoard.board;
     const boardBox = document.querySelectorAll('.boardBox'); 
-    const displayResult = document.querySelector('.displayResult');
-    const modal = document.querySelector('.modal');
-    const exitModal = document.querySelector('.exitModal');
     let counter = 0;
 
     for (let i = 0; i < board.length; i++) {
@@ -49,36 +46,19 @@ const displayController = (() => {
             }
         
             const endScenarios = {
-                filledRow: ((((leftCol.top === midCol.top) && (leftCol.top === rightCol.top)) && leftCol.top !== '') || 
-                            (((leftCol.mid === midCol.mid) && (leftCol.mid === rightCol.mid)) && leftCol.mid !== '') ||
-                            (((leftCol.bot === midCol.bot) && (leftCol.bot === rightCol.bot)) && leftCol.bot !== '')),
-                filledCol: ((((leftCol.top === leftCol.mid) && (leftCol.top === leftCol.bot)) && leftCol.top !== '') || 
-                            (((midCol.top === midCol.mid) && (midCol.top === midCol.bot)) && midCol.top !== '') || 
-                            (((rightCol.top === rightCol.mid) && (rightCol.top === rightCol.bot)) && rightCol.top !== '')),
-                filledDiagonal: ((((leftCol.top === midCol.mid) && (leftCol.top === rightCol.bot)) && leftCol.top !== '') || 
-                                 (((rightCol.top === midCol.mid) && (rightCol.top === leftCol.bot)) && rightCol.top !== '')),
+                filledTopRow: ((leftCol.top === midCol.top) && (leftCol.top === rightCol.top)) && leftCol.top !== '', 
+                filledMidRow: ((leftCol.mid === midCol.mid) && (leftCol.mid === rightCol.mid)) && leftCol.mid !== '',
+                fileldBotRow: ((leftCol.bot === midCol.bot) && (leftCol.bot === rightCol.bot)) && leftCol.bot !== '',
+                filledLeftCol: ((leftCol.top === leftCol.mid) && (leftCol.top === leftCol.bot)) && leftCol.top !== '',
+                filledMidCol: ((midCol.top === midCol.mid) && (midCol.top === midCol.bot)) && midCol.top !== '', 
+                filledRightCol: ((rightCol.top === rightCol.mid) && (rightCol.top === rightCol.bot)) && rightCol.top !== '',
+                RtoLDiagonal: ((leftCol.top === midCol.mid) && (leftCol.top === rightCol.bot)) && leftCol.top !== '', 
+                LtoRDiagonal: ((rightCol.top === midCol.mid) && (rightCol.top === leftCol.bot)) && rightCol.top !== '',
                 noWinner: (leftCol.top != '' && leftCol.mid != '' && leftCol.bot != '' && rightCol.top != '' && rightCol.mid != '' && rightCol.bot != '' && midCol.top != '' && midCol.mid != '' && midCol.bot != '')
             }
 
-            if (endScenarios.filledRow === true || endScenarios.filledCol === true || endScenarios.filledDiagonal === true) { 
-                // Display a modal that displays the who won (X or O)
-                // Modal should have a button that says restart game which clears the gameBoard.board and the board content
-                displayResult.textContent = 'Somebody won'
-                modal.showModal();
-            } else if (endScenarios.noWinner) {
-                displayResult.textContent = 'It was a draw';
-                modal.showModal();
-            }
         })
     }
-
-    exitModal.addEventListener('click', () => {
-        modal.close();
-        for (let i = 0; i < board.length; i++) {
-            board[i] = '';
-            boardBox[i].textContent = board[i];
-        }
-    })
 
 })();
 
